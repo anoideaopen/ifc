57369ce4246e882c0cb2023b26db1df5734d983a
Fedor Partanskiy <fedor.partanskiy@atme.com>
2024-12-14 23:29:58 +0300

    add github action
    Signed-off-by: Fedor Partanskiy <fedor.partanskiy@atme.com>

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,8 @@ var rootCmd = &cobra.Command{
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(version string, commit string, date string) {
+	fmt.Printf("start ifc version - %s, commit - %s,date - %s\n", version, commit, date)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
